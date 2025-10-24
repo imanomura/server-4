88657c4e5e6eff98b9c57ed2e093a0f6f3d3376b
imanomura <206794822+imanomura@users.noreply.github.com>
2025-10-24 12:04:32 +0900

    fetchとserver.js書き込み

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 // 自分で一から書いてみよう
+import { Hono } from 'jsr:@hono/hono';
+import { serveStatic } from 'jsr:@hono/hono/deno';
+
+const app = new Hono();
+
+app.use('/*', serveStatic({ root: './public' }));
+
+Deno.serve(app.fetch);
+
+app.get('/api', (c) => {
+  return c.json({
+    message: 'Hello World!'
+  });
+});
